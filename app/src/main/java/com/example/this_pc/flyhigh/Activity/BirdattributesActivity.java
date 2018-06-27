db16e793b6df99b7655e88e7bcd9c08b0bed58fb
DawnImpulse <dawnimpulse@gmail.com>
2018-06-27 18:04:48 +0530

    UI changes in main activity and final activity

--- app/src/main/java/com/example/this_pc/flyhigh/Activity/BirdattributesActivity.java
+++ app/src/main/java/com/example/this_pc/flyhigh/Activity/BirdattributesActivity.java
@@ -1,29 +1,19 @@
 package com.example.this_pc.flyhigh.Activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.this_pc.flyhigh.R;
 import com.example.this_pc.flyhigh.adapter.AttributesAdapter;
-import com.example.this_pc.flyhigh.interfaces.ApiClient;
-import com.example.this_pc.flyhigh.interfaces.ApiInterface;
 import com.example.this_pc.flyhigh.pojo.Detail_imageFilter;
-import com.example.this_pc.flyhigh.pojo.FilterImageListp;
 import com.example.this_pc.flyhigh.utils.C;
 import com.example.this_pc.flyhigh.utils.Listdetails;
-
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class BirdattributesActivity extends AppCompatActivity {
 
@@ -43,7 +33,7 @@ public class BirdattributesActivity extends AppCompatActivity {
         details= Listdetails.detail_imageFilter;
         Log.d("keys values",details.toString());
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.attributes_list);
-        recyclerView.setLayoutManager(new GridLayoutManager(BirdattributesActivity.this,2,LinearLayoutManager.VERTICAL,false));
+        recyclerView.setLayoutManager(new GridLayoutManager(BirdattributesActivity.this,1,LinearLayoutManager.VERTICAL,false));
         recyclerView.setAdapter(new AttributesAdapter(details));
 
     }
